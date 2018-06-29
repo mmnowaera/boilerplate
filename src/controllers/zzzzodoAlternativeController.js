83c973ffe4f03522b8158c61ecf9cc2035283d32
m.maciejewski <mmnowaera@gmail.com>
2018-06-29 11:00:28 +0200

    Remove unnesesary code

--- src/controllers/zzzzodoAlternativeController.js
+++ src/controllers/zzzzodoAlternativeController.js
@@ -42,20 +42,6 @@
             }
         }
     };
-    app.TodoController = class TodoController extends app.AbstractTodoController {
-        constructor(model) {
-            super(model);
-        }
-        addItem(name) {
-            super.addItem(name);
-        }
-        removeItem(id) {
-            super.removeItem(id);
-        }
-        toggleItemStatus(id) {
-            super.toggleItemStatus(id);
-        }
-    };
 
     app.TodoAlternativeController = class TodoAlternativeController extends app.AbstractTodoController {
         constructor(model) {
@@ -64,42 +50,23 @@
         }
         addItem(name) {
             if (app.TodoItemModel.isValid(name)) {
-                // app.repositories.http.save({name, status: 'unresolved'})
-                //     .then(data => {
-                        const newModel = app.TodoItemModel.fromJSON({
-                            name,
-                            status: 'unresolved',
-                            id: this.i++,
-                        });
-                        this.model.setItem(newModel);
-                    // })
-                    // .catch(err => {
-                    //     console.error(err);
-                    // });
+                const newModel = app.TodoItemModel.fromJSON({
+                    name,
+                    status: 'unresolved',
+                    id: this.i++,
+                });
+                this.model.setItem(newModel);
             } else {
                 console.error('client: nie dziala dodawanie');
             }
         }
         removeItem(id) {
-            // app.repositories.http.remove(id)
-            //     .then(() => {
-                    this.model.removeById(parseInt(id));
-                // })
-                // .catch(err => {
-                //     console.error(err);
-                // });
+            this.model.removeById(parseInt(id));
         }
         toggleItemStatus(id) {
             const itemModel = this.model.findItemById(id);
             if (itemModel) {
-                // const status = (itemModel.get('status') === 'done') ? 'unresolved' : 'done';
-                // app.repositories.http.updateItemById({name: itemModel.get('name'), id, status})
-                //     .then(() => {
-                        itemModel.toggleStatus();
-                // })
-                // .catch(err => {
-                //     console.error(err);
-                // });
+                itemModel.toggleStatus();
             }
         }
     };
